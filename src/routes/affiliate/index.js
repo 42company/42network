6a54c42239f920af9d4e1a9f64d2de7a4aed5afb
cigon <yoosung@ultracaption.net>
2017-05-15 14:41:42 +0900

    affiliate design

--- src/routes/affiliate/index.js
+++ src/routes/affiliate/index.js
@@ -7,22 +7,118 @@ export default class Affiliate extends React.Component {
     return (
       <Layout>
         <section className={style['banner']}>
-          <div className={style['banner-image']} />
-          <div className={style['banner-description']}>
-            <div className={style['banner-description-head']}>You are mobile publisher,ad network or media buyer!</div>
-            <div className={style['banner-description-body']}>We help you monetise your traffic!</div>
+          <div className={style['section-container']}>
+            <div className={style['banner-image']} />
+            <div className={style['banner-description']}>
+              <h1 className={style['banner-description-head']}>You are mobile App or a Mobile Service Provider!</h1>
+              <p className={style['banner-description-body']}>We deliver users & performance</p>
+            </div>
           </div>
         </section>
         <section className={style['contact']}>
-          <div>
+          <div className={style['section-container']}>
+            <div className={style['flex-box']}>
+              <div className={style['flex-description-content-container']}>
+                <div className={style['ic-mobile']} />
+                <div className={style['description-wrapper']}>
+                  <h3 className={style['contact-header']}>
+                    MOBILE APPS
+                  </h3>
+                  <p className={style['description-body']}>
+                    We create value for your app depending on your growth phase.
+                    Whether it be installs or more. We aim at bringing you quality users and a clear ROI.
+                    Our expert teams help with best solutions for optimisation and most efficient return.
+                    Our knowledge of app mechanics helps us target the right users.
+                  </p>
+                </div>
+                <div className={style['divider']} />
+                <div className={style['ic-provider']} />
+                <div className={style['description-wrapper']}>
+                  <h3 className={style['contact-header']}>
+                    MOBILE SERVICE PROVIDERS
+                  </h3>
+                  <p className={style['description-body']}>
+                    We believe in delivering performance, be it CPI,CPL,CPM,CPC,CPA.
+                    You name it, we perform and deliver. Our traffic is good quality
+                    and we tie up with the best publishers to ensure the response
+                    is most efficient and as per your desired KPIs. We understand your
+                    business needs and partner with you as a network to achieve the
+                    best with maximum and best quality reach within performance parameters.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={style['flex-box']}>
+              <div className={style['flex-content-container']}>
+                <form>
+                  <h3 className={style['contact-header']}>CONTACT US</h3>
+                  <label htmlFor='name'>Name</label>
+                  <input type='text' id='name' name='name' />
+                  <label htmlFor='name'>Company</label>
+                  <input type='text' id='company' name='company' />
+                  <label htmlFor='name'>Email</label>
+                  <input type='text' id='e-mail' name='e-mail' />
+                  <label htmlFor='name'>Phone number</label>
+                  <input type='text' id='phone-number' name='phone-number' />
+                  <label htmlFor='name'>Skype ID</label>
+                  <input type='text' id='skype' name='skype' />
+                  <input className={style['submit']} type='submit' value='Submit' />
+                </form>
+              </div>
+            </div>
           </div>
         </section>
         <section className={style['benefits']}>
-          <div>
+          <div className={style['section-container']}>
+            <div className={style['flex-box']}>
+              <div className={style['flex-content-container']}>
+                <div className={style['ic-quality']} />
+                <h3 className={style['benefits-header']}>QUALITY</h3>
+                <p className={style['benefits-description']}>
+                  - Key Performance areas can be set by you<br />
+                  - Traffic Targeting<br />
+                  - Optimisation
+                </p>
+              </div>
+            </div>
+            <div className={style['flex-box']}>
+              <div className={style['flex-content-container']}>
+                <div className={style['ic-technology']} />
+                <h3 className={style['benefits-header']}>TECHNOLOGY</h3>
+                <p className={style['benefits-description']}>
+                  - Real time dashboard<br />
+                  - Optimisation as per performance result<br />
+                  - Budget Management<br />
+                  - Innovation development team at support for special campaigns
+                </p>
+              </div>
+            </div>
+            <div className={style['flex-box']}>
+              <div className={style['flex-content-container']}>
+                <div className={style['ic-quantity']} />
+                <h3 className={style['benefits-header']}>QUANTITY</h3>
+                <p className={style['benefits-description']}>
+                  - All India reach with localisation possible<br />
+                  - Wide variety of traffic services<br />
+                  - Wide variety of ad formats
+                </p>
+              </div>
+            </div>
+            <div className={style['flex-box']}>
+              <div className={style['flex-content-container']}>
+                <div className={style['ic-support']} />
+                <h3 className={style['benefits-header']}>DEDICATED SUPPORT</h3>
+                <p className={style['benefits-description']}>
+                  -Dedicated account managers for quick connect<br />
+                  -Partner with you for optimisation for maximum ROI
+                </p>
+              </div>
+            </div>
           </div>
         </section>
         <section className={style['slogan']}>
-          <div>
+          <div className={style['section-container']}>
+            <h1>We deliver. That’s a promise!<br />Collaborate with 42Network and see your business grow</h1>
           </div>
         </section>
       </Layout>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import style from './home.css'
-import introImage from './images/img_intro.png'
+import introImage from './images/img_intro_2.png'
 
 export default class Home extends React.Component {
   render () {
@@ -9,49 +9,61 @@ export default class Home extends React.Component {
       <Layout>
         <div className={style['root']}>
           <section className={style['intro']}>
-            <div className={style['intro-container']}>
+            <div className={style['section-container']}>
               <h1 className={style['intro-header']}>42network for online mobile performance</h1>
               <div>
                 <button className={style['normal-affiliate-button']}>Affiliate</button>
                 <button className={style['normal-advertiser-button']}>Advertiser</button>
               </div>
-              <img className={style['intro-image']} src={introImage} width='100%' alt='42network-intro' />
             </div>
+            <img className={style['intro-image']} src={introImage} width='100%' alt='42network-intro' />
           </section >
           <section className={style['description']}>
-            <h1 className={style['description-header']}>The Answer to the Ultimate Question of<br />Life, The Universe, and Everything:</h1>
-            <div className={style['icon']} />
-            <p className={style['description-body']}>
-              <b>42Company India Pvt Ltd</b> is a fast growing online media company focussing on mobile entertainment services in India.
-              Founded by mobile industry veterans, privately funded, we are an independent, owner-managed company.
-              We have a dedicated team of professionals looking at analysing your advertising needs and providing best solutions.
-            </p>
+            <div className={style['section-container']}>
+              <h1 className={style['description-header']}>The Answer to the Ultimate Question of<br />Life, The Universe, and Everything:</h1>
+              <div className={style['icon']} />
+              <p className={style['description-body']}>
+                <b>42Company India Pvt Ltd</b> is a fast growing online media company focussing on mobile entertainment services in India.
+                Founded by mobile industry veterans, privately funded, we are an independent, owner-managed company.
+                We have a dedicated team of professionals looking at analysing your advertising needs and providing best solutions.
+              </p>
+            </div>
           </section>
           <section className={style['diagram-bg']}>
-            <div className={style['diagram']} />
-            <div className={style['be-a-partner']}>
-              <div className={style['affiliates']}>
-                <h1 className={style['partner-header']}>Affiliates</h1>
-                <div className={style['partner-description']}>You are publisher, ad network or media buyer. We deliver local monetisation</div>
-                <button className={style['affiliate-button']}>42 for Affiliates</button>
-              </div>
-              <div className={style['advertisers']}>
-                <h1 className={style['partner-header']}>Advertiser</h1>
-                <div className={style['partner-description']}>You are a mobile service provider or app developer. We deliver users, distribution and sales.</div>
-                <button className={style['advertiser-button']}>42 for Advertiser</button>
+            <div className={style['section-container']}>
+              <div className={style['diagram']} />
+              <div className={style['be-a-partner']}>
+                <div className={style['affiliates']}>
+                  <div className={style['flex-container']}>
+                    <h1 className={style['partner-header']}>Affiliates</h1>
+                    <div className={style['partner-description']}>You are publisher, ad network or media buyer. We deliver local monetisation</div>
+                    <button className={style['affiliate-button']}>42 for Affiliates</button>
+                  </div>
+                </div>
+                <div className={style['advertisers']}>
+                  <div className={style['flex-container']}>
+                    <h1 className={style['partner-header']}>Advertiser</h1>
+                    <div className={style['partner-description']}>You are a mobile service provider or app developer. We deliver users, distribution and sales.</div>
+                    <button className={style['advertiser-button']}>42 for Advertiser</button>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
           <section className={style['slogan']}>
-            <h1>We deliver. That’s a promise!</h1>
-            <p className={style['slogan-description']}>Collaborate with 42Network and see your business grow</p>
+            <div className={style['section-container']}>
+              <h1>We deliver. That’s a promise!</h1>
+              <p className={style['slogan-description']}>Collaborate with 42Network and see your business grow</p>
+            </div>
           </section>
           <section className={style['partners']}>
-            <div className={style['partners-header']}>
-              Our Advertising partners
-            </div>
-            <div className={style['partners-header']}>
-              Our Publisher partners
+            <div className={style['section-container']}>
+              <div className={style['partners-header']}>
+                Our Advertising partners
+              </div>
+              <div className={style['partners-header']}>
+                Our Publisher partners
+              </div>
             </div>
           </section>
         </div>
